9e6afaefe79b61a45a65e76dfbe4972897fc4799
Alexy Martinet <amlds.dev@gmail.com>
2023-03-03 22:35:33 +0100

    validity date

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :event_type
   belongs_to :school
+  validate :start_time_cant_be_end_time
+
+  def start_time_cant_be_end_time
+    if start_time > end_time && start_time == end_time
+      errors.add(:start_time, "can't be the same as end time")
+    end
 end
