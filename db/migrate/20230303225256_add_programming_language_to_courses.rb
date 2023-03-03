080951c447f002cdbb381b381aa043df21a1cab6
Alexy Martinet <amlds.dev@gmail.com>
2023-03-04 00:27:54 +0100

    Create new seed & new migrate

--- db/migrate/20230303225256_add_programming_language_to_courses.rb
+++
@@ -1,5 +0,0 @@
-class AddProgrammingLanguageToCourses < ActiveRecord::Migration[7.0]
-  def change
-    add_column :courses, :programming_language, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_225256) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_231043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_225256) do
     t.bigint "course_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "programming_language"
     t.index ["course_id"], name: "index_challenges_on_course_id"
   end
 
@@ -41,10 +42,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_225256) do
   end
 
   create_table "courses", force: :cascade do |t|
-    t.string "type"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "programming_language"
   end
 
   create_table "event_types", force: :cascade do |t|
