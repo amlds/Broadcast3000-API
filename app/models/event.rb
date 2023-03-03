bb1ee94286184696fce45389057fc105f50ff92d
Alexy Martinet <amlds.dev@gmail.com>
2023-03-03 22:39:51 +0100

    modify function validity date

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,10 @@ class Event < ApplicationRecord
   validate :start_time_cant_be_end_time
 
   def start_time_cant_be_end_time
-    if start_time > end_time && start_time == end_time
-      errors.add(:start_time, "can't be the same as end time")
+    if start_time > end_time
+      errors.add(:start_time, 'cannot be after end time')
+    elsif start_time == end_time
+      errors.add(:start_time, 'cannot be the same end time')
     end
+  end
 end
