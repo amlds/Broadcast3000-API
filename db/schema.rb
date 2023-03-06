9a9b7bb47f050c13556fa5bef23d8c3365105129
Alexy Martinet <amlds.dev@gmail.com>
2023-03-06 19:18:14 +0100

    new migration shcool

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_095117) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_175716) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_095117) do
     t.bigint "city_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "message_display"
+    t.integer "nbr_carrousel"
     t.index ["city_id"], name: "index_schools_on_city_id"
   end
 
