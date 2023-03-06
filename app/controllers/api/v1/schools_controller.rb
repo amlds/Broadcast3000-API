85d9998a184d695a8aaed73925c17281f837162d
Alexy Martinet <amlds.dev@gmail.com>
2023-03-06 19:24:53 +0100

    FIX: bug update

--- app/controllers/api/v1/schools_controller.rb
+++ app/controllers/api/v1/schools_controller.rb
@@ -8,4 +8,28 @@ class Api::V1::SchoolsController < ApplicationController
     @school = School.find(params[:id])
     render json: @school
   end
+
+  def create
+    @school = School.new(school_params)
+    if @school.save
+      render json: @school, status: :created
+    else
+      render json: @school.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @school = School.find(params[:id])
+    if @school.update(school_params)
+      render json: @school
+    else
+      render json: @school.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def school_params
+    params.require(:school).permit(:display_path, :message_display, :nbr_carrousel)
+  end
 end
