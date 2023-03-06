b103955b121d1f971890152b7b3a0edf49f47428
Alexy Martinet <amlds.dev@gmail.com>
2023-03-06 11:28:57 +0100

    modify controller

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::ChallengesController < ApplicationController
   def index
     @course = Course.find(params[:course_id])
+    p @course
     @challenges = @course.challenges
+    ap @challenges
     render json: @challenges
   end
 
