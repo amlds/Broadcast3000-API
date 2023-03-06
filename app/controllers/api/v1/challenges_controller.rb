1001a574b8fdfc019f11ac330c81444c9d925f15
Alexy Martinet <amlds.dev@gmail.com>
2023-03-06 11:23:02 +0100

    add controller INDEX / SHOW

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::ChallengesController < ApplicationController
+  def index
+    @course = Course.find(params[:course_id])
+    @challenges = @course.challenges
+    render json: @challenges
+  end
+
+  def show
+    @course = Course.find(params[:course_id])
+    @challenge = @course.challenges.find(params[:id])
+    render json: @challenge
+  end
 end
