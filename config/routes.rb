1b8c36be84989fbef7d88db25dfde4bb88c1000b
Alexis Barreirinho <alexis@barreirinho.com>
2023-03-28 16:28:51 +0200

    Create action cable server routes. create channel file. setup render on new events

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,19 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      # Action cable server
+      mount ActionCable.server => "/cable"
+
+      # Authentication
       post "auth/login", to: "authentications#login"
+
+      # Display
       get "display/:display_path", to: "schools#display"
 
+      # Users
       resources :users
+
+      # Schools
       resources :schools, only: %i[index show create update] do
         resources :events, only: %i[index update create destroy]
         resources :batchs, only: %i[index update create destroy]
