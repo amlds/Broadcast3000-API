6f9c0f5d54541ca7d303a73313ad44d65dabb1ae
Alexis Barreirinho <alexis@barreirinho.com>
2023-04-07 11:17:36 +0200

    Make seed for data course with date on challenges

--- db/seeds.rb
+++ db/seeds.rb
@@ -90,37 +90,51 @@ Challenge.create!(name: "Models & CRUD", programming_language: "Ruby on Rails", 
 Challenge.create!(name: "Demo Day", programming_language: "Ruby on Rails", course: Course.first)
 
 p "Create Challenge data"
-Challenge.create!(name: "Python", programming_language: "Software", course: Course.last)
-Challenge.create!(name: "Programming Basics", programming_language: "Python", course: Course.last)
-Challenge.create!(name: "Flow, Conditionals & Arrays", programming_language: "Python", course: Course.last)
-Challenge.create!(name: "Iterators & Blocks", programming_language: "Python", course: Course.last)
-Challenge.create!(name: "Hash & Symbols", programming_language: "Python", course: Course.last)
-Challenge.create!(name: "Regular Expressions", programming_language: "Python", course: Course.last)
-Challenge.create!(name: "Parsing", programming_language: "Python", course: Course.last)
-Challenge.create!(name: "Classes & Instances", programming_language: "Python", course: Course.last)
-Challenge.create!(name: "Inheritance & Self", programming_language: "Python", course: Course.last)
-Challenge.create!(name: "Schema Desing & SQL", programming_language: "SQL", course: Course.last)
-Challenge.create!(name: "CRUD with SQL", programming_language: "SQL", course: Course.last)
-Challenge.create!(name: "Active Record Basics", programming_language: "SQL", course: Course.last)
-Challenge.create!(name: "Associations & Validations", programming_language: "SQL", course: Course.last)
-Challenge.create!(name: "HTML & CSS", programming_language: "HTML & CSS", course: Course.last)
-Challenge.create!(name: "CSS Components", programming_language: "HTML & CSS", course: Course.last)
-Challenge.create!(name: "Bootstrap & Layouts", programming_language: "HTML & CSS", course: Course.last)
-Challenge.create!(name: "JavaScript basics", programming_language: "javascript", course: Course.last)
-Challenge.create!(name: "DOM & Events", programming_language: "javascript", course: Course.last)
-Challenge.create!(name: "HTTP & AJAX", programming_language: "javascript", course: Course.last)
-Challenge.create!(name: "JavaScript Packages", programming_language: "javascript", course: Course.last)
-Challenge.create!(name: "Routing, Controllers & Views", programming_language: "Python on Rails", course: Course.last)
-Challenge.create!(name: "Models & CRUD", programming_language: "Python on Rails", course: Course.last)
-Challenge.create!(name: "Associations & Validations", programming_language: "Python on Rails", course: Course.last)
-Challenge.create!(name: "Authentication & Devise", programming_language: "Python on Rails", course: Course.last)
-Challenge.create!(name: "Authorization & Pundit", programming_language: "Python on Rails", course: Course.last)
-Challenge.create!(name: "Geocoding", programming_language: "Python on Rails", course: Course.last)
-Challenge.create!(name: "Search", programming_language: "Python on Rails", course: Course.last)
-Challenge.create!(name: "AJAX in Rails", programming_language: "Python on Rails", course: Course.last)
-Challenge.create!(name: "WebSocket & Action Cable", programming_language: "Python on Rails", course: Course.last)
-Challenge.create!(name: "Background Jobs & Sidekiq", programming_language: "Python on Rails", course: Course.last)
-
+Challenge.create!(name: "Setup", programming_language: "Software", course: Course.first)
+Challenge.create!(name: "Programming Basics", programming_language: "ruby", course: Course.first)
+Challenge.create!(name: "Flow, Conditionals & Arrays", programming_language: "ruby", course: Course.first)
+Challenge.create!(name: "Iterators & Blocks", programming_language: "ruby", course: Course.first)
+Challenge.create!(name: "Hash & Symbols", programming_language: "ruby", course: Course.first)
+Challenge.create!(name: "Regular Expressions", programming_language: "ruby", course: Course.first)
+Challenge.create!(name: "Parsing", programming_language: "ruby", course: Course.first)
+Challenge.create!(name: "Classes & Instances", programming_language: "ruby", course: Course.first)
+Challenge.create!(name: "Inheritance & Self", programming_language: "ruby", course: Course.first)
+Challenge.create!(name: "Cookbook (day 1)", programming_language: "ruby", course: Course.first)
+Challenge.create!(name: "Cookbook (day 2)", programming_language: "ruby", course: Course.first)
+Challenge.create!(name: "Food Delivery (day 1)", programming_language: "ruby", course: Course.first)
+Challenge.create!(name: "Food Delivery (day 2)", programming_language: "ruby", course: Course.first)
+Challenge.create!(name: "Schema Desing & SQL", programming_language: "SQL", course: Course.first)
+Challenge.create!(name: "CRUD with SQL", programming_language: "SQL", course: Course.first)
+Challenge.create!(name: "Active Record Basics", programming_language: "SQL", course: Course.first)
+Challenge.create!(name: "Associations & Validations", programming_language: "SQL", course: Course.first)
+Challenge.create!(name: "HTML & CSS", programming_language: "HTML & CSS", course: Course.first)
+Challenge.create!(name: "CSS Components", programming_language: "HTML & CSS", course: Course.first)
+Challenge.create!(name: "Bootstrap & Layouts", programming_language: "HTML & CSS", course: Course.first)
+Challenge.create!(name: "JavaScript basics", programming_language: "javascript", course: Course.first)
+Challenge.create!(name: "DOM & Events", programming_language: "javascript", course: Course.first)
+Challenge.create!(name: "HTTP & AJAX", programming_language: "javascript", course: Course.first)
+Challenge.create!(name: "JavaScript Packages", programming_language: "javascript", course: Course.first)
+Challenge.create!(name: "Product Design Sprint", programming_language: "Figma", course: Course.first)
+Challenge.create!(name: "Routing, Controllers & Views", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Models & CRUD", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Advanced Routing", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Front-End", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Hosting & Image Upload", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Authentication & Devise", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Authorization & Pundit", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Geocoding", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Search", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "AJAX in Rails", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "WebSocket & Action Cable", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Background Jobs & Sidekiq", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "DB Advanced", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Demo Preparation", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Project (Day 6)", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Project (Day 7)", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Project (Day 8)", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Dress Rehearsal", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Models & CRUD", programming_language: "Ruby on Rails", course: Course.first)
+Challenge.create!(name: "Demo Day", programming_language: "Ruby on Rails", course: Course.first)
 p "Finish seed !"
 
 # associate a challenge to a day of the week from a start date
@@ -129,6 +143,25 @@ day = 0
 saturday = 5
 
 Challenge.where(course: Course.first).each do |challenge|
+  if day.zero?
+    challenge.update!(day: start_date)
+    day += 1
+  elsif (day % saturday).zero? # if day is saturday, we skip to monday
+    day += 2
+    challenge.update!(day: start_date + day.day)
+    saturday += 7
+    day += 1
+  else
+    challenge.update!(day: start_date + day.day)
+    day += 1
+  end
+end
+
+start_date = Date.new(2023, 4, 17)
+day = 0
+saturday = 5
+
+Challenge.where(course: Course.last).each do |challenge|
   if day.zero?
     challenge.update!(day: start_date)
     day += 1
